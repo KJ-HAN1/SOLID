80f97f6854c5d3b01a0f4f7ce5470bfebda7cc0e
KJ-HAN1 <hkjin@kakao.com>
2025-07-11 10:45:14 +0900

    add OCP violation

--- src/main/java/org/example/SOLID/v2/service/PurchaseService.java
+++ src/main/java/org/example/SOLID/v2/service/PurchaseService.java
@@ -11,7 +11,20 @@ public class PurchaseService {
         int discount = user.getDiscount().discount(product);
         return product.getPrice() - discount;
     }
-
+    //OCP 위반 예시 -> User의 할인 등급을 String으로 입력해줬다는 가정
+    // --> 새로운 등급을 추가하려면 기존 switch문을 수정해야함! OCP 위반
+//    String grade = user.getgrade();
+//
+//    switch (grade) {
+//        case "Silver":
+//            discount = product.getPrice() / 10;
+//            break;
+//        case "Gold":
+//            discount = product.getPrice() / 5;
+//            break;
+//        default:
+//            discount = 0;
+//    }
 
     //DIP 위반 예시 -> Discount 인터페이스를 구현한 SilverDiscount를 직접 의존
 //    public int calculateSilverDiscount(User user, Product product) {
